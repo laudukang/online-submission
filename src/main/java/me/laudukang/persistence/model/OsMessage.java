d72d02905e9bb3648407b5f1809914465abe397f
laudukang <laudukang@gmail.com>
2016-02-25 16:13:16 +0800

    rename OsChat to OsMessage

---
+++ src/main/java/me/laudukang/persistence/model/OsMessage.java
@@ -0,0 +1,97 @@
+package me.laudukang.persistence.model;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+/**
+ * The persistent class for the os_message database table.
+ */
+@Entity
+@Table(name = "os_message")
+@NamedQuery(name = "OsMessage.findAll", query = "SELECT o FROM OsMessage o")
+public class OsMessage implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Lob
+    @Column(columnDefinition = "text")
+    private String content;
+
+    @Column(name = "post_time")
+    private Timestamp postTime;
+
+    private String title;
+
+    // bi-directional many-to-one association to OsAdmin
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "admin_id", nullable = true)
+    private OsAdmin osAdmin;
+
+    // bi-directional many-to-one association to OsUser
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = true)
+    private OsUser osUser;
+
+    public OsMessage() {
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getPostTime() {
+        return this.postTime;
+    }
+
+    public void setPostTime(Timestamp postTime) {
+        this.postTime = postTime;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public OsAdmin getOsAdmin() {
+        return this.osAdmin;
+    }
+
+    public void setOsAdmin(OsAdmin osAdmin) {
+        this.osAdmin = osAdmin;
+    }
+
+    public OsUser getOsUser() {
+        return this.osUser;
+    }
+
+    public void setOsUser(OsUser osUser) {
+        this.osUser = osUser;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE, true);
+    }
+}
